cd55d1ef08cc6d495e0ba5b09464aa466283e4de
Takumi <yamsan3610@gmail.com>
2021-04-30 02:20:15 +0900

    [Add] taskモデルのテストを追加

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :task do
-    
+    title    {"test"}
+    content  {"content"}
+    status   {:todo}
+    deadline {1.days.from_now}
+    association :user
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'varidation' do
+    it 'is valid with all attributes' do
+      task = build(:task)
+      expect(task).to be_valid
+      expect(task.errors).to be_empty
+    end
+
+    it 'is invalid without title' do
+      task = build(:task, title: nil)
+      expect(task).to be_invalid
+      expect(task.errors[:title]).to include("can't be blank")
+    end
+
+    it 'is invalid without status' do
+      task = FactoryBot.build(:task, status: nil)
+      expect(task).to be_invalid
+      expect(task.errors[:status]).to include("can't be blank")
+    end
+
+    it 'is invalid with a duplicate title' do
+      create(:task)
+      task = build(:task)
+      expect(task).to be_invalid
+      expect(task.errors[:title]).to include("has already been taken")
+    end
+
+    it 'is valid with another title' do
+      create(:task)
+      task = build(:task, title: 'hoge')
+      expect(task).to be_valid
+      expect(task.errors[:title]).to_not include("has already been taken")
+    end
+    #"User must exist"
+  end
 end
