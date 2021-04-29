e29c6fbce91d0acf362daca303eaefe0de7461fe
Takumi <yamsan3610@gmail.com>
2021-04-30 03:01:58 +0900

    [Fix] 内容がわかるように変数名を変更

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -9,30 +9,29 @@ RSpec.describe Task, type: :model do
     end
 
     it 'is invalid without title' do
-      task = build(:task, title: nil)
-      expect(task).to be_invalid
-      expect(task.errors[:title]).to include("can't be blank")
+      task_without_title = build(:task, title: nil)
+      expect(task_without_title).to be_invalid
+      expect(task_without_title.errors[:title]).to include("can't be blank")
     end
 
     it 'is invalid without status' do
-      task = FactoryBot.build(:task, status: nil)
-      expect(task).to be_invalid
-      expect(task.errors[:status]).to include("can't be blank")
+      task_without_status = FactoryBot.build(:task, status: nil)
+      expect(task_without_status).to be_invalid
+      expect(task_without_status.errors[:status]).to include("can't be blank")
     end
 
     it 'is invalid with a duplicate title' do
       create(:task)
-      task = build(:task)
-      expect(task).to be_invalid
-      expect(task.errors[:title]).to include("has already been taken")
+      task_with_duplicate_title = build(:task)
+      expect(task_with_duplicate_title).to be_invalid
+      expect(task_with_duplicate_title.errors[:title]).to include("has already been taken")
     end
 
     it 'is valid with another title' do
       create(:task)
-      task = build(:task, title: 'hoge')
-      expect(task).to be_valid
-      expect(task.errors[:title]).to_not include("has already been taken")
+      task_with_another_title = build(:task, title: 'hoge')
+      expect(task_valid_with_another_title).to be_valid
+      expect(task_valid_with_another_title.errors[:title]).to_not include("has already been taken")
     end
-    #"User must exist"
   end
 end
