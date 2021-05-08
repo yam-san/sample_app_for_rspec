4d90a8a9e7c6bd1c33895d3540d64aa391ed5ea2
Takumi <yamsan3610@gmail.com>
2021-05-08 15:47:20 +0900

    [Modify] ログインマクロの修正

--- spec/support/login_macros.rb
+++ spec/support/login_macros.rb
@@ -3,7 +3,7 @@ module LoginMacro
     visit root_path
     click_link 'Login'
     fill_in 'email', with: user.email
-    fill_in 'password', with: user.password
+    fill_in 'password', with: 'password'
     click_button 'Login'
   end
 end
